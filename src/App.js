904ee98317c77ed324ff7eac4166e3782a7b30fc
shandamckinney <mckinney.shanda@gmail.com>
2021-01-08 17:43:05 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState(0);
+  const [listOfRecipes, setListOfRecipes] = useState([]);
+
+  const addRecipe = () => {
+    Axios.post('http://localhost:3001/addrecipe', {name: name, rating: rating}).then(() => {
+      alert('yes');
+    }).catch(() => {
+      alert('nope');
+    });
+  };
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/read', {name: name, rating: rating})
+    .then((response) => {
+     setListOfRecipes(response.data);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <input type="text" placeholder="recipe name" onChange={(e) => {setName(e.target.value)}}/>
+        <input type="number" placeholder="recipe rating" onChange={(e) => {setRating(e.target.value)}}/>
+
+        <button onClick={(addRecipe)}>Add Recipe</button>
+      </div>
     </div>
   );
 }
